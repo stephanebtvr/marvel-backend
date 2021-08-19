e5a6813096f14c2fd0e687ad0f4f0da33312d220
Goldstandard <bettaver.stephane@gmail.com>
2021-08-19 21:59:02 +0200

    second commit

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const formidable = require("express-formidable");
+const app = express();
+const cors = require("cors");
+const axios = require("axios");
+require("dotenv").config();
+app.use(formidable());
+app.use(cors());
+
+app.get("/characters", async (req, res) => {
+  try {
+    const limit = req.query.limit || 100;
+    const skip = req.query.skip || 0;
+
+    let name = "";
+    if (req.query.name) {
+      name = req.query.name;
+    }
+
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.MARVEL_API_KEY}&limit=${limit}&skip=${skip}&name=${name}`
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+app.get("/comics/:characterId", async (req, res) => {
+  try {
+    let characterId = "";
+    if (req.params.characterId) {
+      characterId = req.params.characterId;
+    }
+
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics/${characterId}?apiKey=${process.env.MARVEL_API_KEY}`
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+app.get("/comics", async (req, res) => {
+  try {
+    const limit = req.query.limit || 100;
+    const skip = req.query.skip || 0;
+
+    let title = "";
+    if (req.query.title) {
+      title = req.query.title;
+    }
+
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.MARVEL_API_KEY}&limit=${limit}&skip=${skip}&title=${title}`
+    );
+    console.log(response.data);
+    res.json(response.data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+app.all("*", (req, res) => {
+  res.status(400).json({ message: "Page not found" });
+});
+
+app.listen(process.env.port, () => {
+  console.log("Server Started");
+});
